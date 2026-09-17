5b5a6a2841e0fa6599e8638c16116b6382200ce5
Taufik Iqbal Ramdhani <taufikiqbalr@gmail.com>
2026-09-17 13:40:32 +0700

    fix: order safety intelligence migration after safety events

--- src/database/migrations/2026091706000-SafetyIntelligence.ts
+++ src/database/migrations/2026091706000-SafetyIntelligence.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class SafetyIntelligence2026091704000 implements MigrationInterface {
-  name = 'SafetyIntelligence2026091704000';
+export class SafetyIntelligence2026091706000 implements MigrationInterface {
+  name = 'SafetyIntelligence2026091706000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
